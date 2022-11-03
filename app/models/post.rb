d29aec676f1330be74f34abd0d13e81f37049d1b
Kim <rodrigkim@gmail.com>
2022-11-03 13:24:59 -0400

    commit tree 38ce2835b4269ee33f2c4b793a2565bfded245a9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, allow_blank: false
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"] }
+    validate :title_is_clickbait
+
+
+    def title_is_clickbait
+        if title
+            if !(title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess"))
+                errors.add(:title, "must be more clickbaity")
+            end
+        end
+    end
 end
